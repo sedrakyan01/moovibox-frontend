c167fb91aff3bb9f6b356fbcf20bb9c7f34edebd
sedrakyan01 <sedrakyan919@gmail.com>
2026-01-25 23:32:45 +0400

    feat(added some lib for tests): vitest, Playwright added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	test: {
+		environment: 'jsdom',
+		globals: true,
+		setupFiles: './src/test/setup.ts',
+		coverage: {
+			reporter: ['text', 'html'],
+			lines: 80,
+			functions: 80,
+			branches: 70,
+			statements: 80,
+		},
+	},
 })
